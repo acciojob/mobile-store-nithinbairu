59f15f84a5802a5e04e739852d0c2c70f9b1363d
nithinbairu <nithinbairu08@gmail.com>
2025-11-17 23:20:41 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,95 +1,12 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import ProductList from "./ProductList";
 import ProductDetails from "./ProductDetails";
 import AdminPanel from "./AdminPanel";
 import EditProduct from "./EditProduct";
 
-// import { mobiles as  } from "./data";
-
-const initialData = [
-  {
-    id: 1,
-    name: "iPhone 9",
-    price: 54900,
-    brand: "Apple",
-    color: "Black",
-    image: "https://dummyjson.com/image/i/products/1/1.jpg",
-    description: "An apple mobile which is nothing like apple. An apple mobile which is nothing like apple. An apple mobile which is nothing like apple.An apple mobile which is nothing like apple. An apple mobile which is nothing like apple. An apple mobile which is nothing like apple."
-  },
-  {
-    id: 2,
-    name: "Samsung Galaxy Book",
-    price: 149999,
-    brand: "Samsung",
-    color: "Silver",
-    image: "https://dummyjson.com/image/i/products/2/1.jpg",
-    description: "Samsung's new variant of the Galaxy Book series. Samsung's new variant of the Galaxy Book series. Samsung's new variant of the Galaxy Book series.Samsung's new variant of the Galaxy Book series. Samsung's new variant of the Galaxy Book series. Samsung's new variant of the Galaxy Book series."
-  },
-  {
-    id: 3,
-    name: "OPPOF19",
-    price: 28000,
-    brand: "OPPO",
-    color: "Blue",
-    image: "https://dummyjson.com/image/i/products/3/1.jpg",
-    description: "OPPO F19 is officially announced on April 2021.OPPO F19 is officially announced on April 2021.OPPO F19 is officially announced on April 2021.OPPO F19 is officially announced on April 2021.OPPO F19 is officially announced on April 2021.OPPO F19 is officially announced on April 2021."
-  },
-  {
-    id: 4,
-    name: "Huawei P30",
-    price: 49999,
-    brand: "Huawei",
-    color: "Black",
-    image: "https://dummyjson.com/image/i/products/4/1.jpg",
-    description: "Huawei P30 is officially announced in March 2019 and was released in April 2019.Huawei P30 is officially announced in March 2019 and was released in April 2019.Huawei P30 is officially announced in March 2019 and was released in April 2019.Huawei P30 is officially announced in March 2019 and was released in April 2019."
-  },
-  {
-    id: 5,
-    name: "MacBook Pro",
-    price: 174999,
-    brand: "Apple",
-    color: "Silver",
-    image: "https://dummyjson.com/image/i/products/5/1.jpg",
-    description: "MacBook Pro 2021 with mini-LED display may launch between September, November.MacBook Pro 2021 with mini-LED display may launch between September, November.MacBook Pro 2021 with mini-LED display may launch between September, November.MacBook Pro 2021 with mini-LED display may launch between September, November."
-  },
-  {
-    id: 6,
-    name: "Microsoft Surface Laptop 4",
-    price: 149999,
-    brand: "Microsoft",
-    color: "Gray",
-    image: "https://dummyjson.com/image/i/products/6/1.jpg",
-    description: "Microsoft Surface Laptop 4 is a powerful and versatile laptop designed for productivity and creativity.Microsoft Surface Laptop 4 is a powerful and versatile laptop designed for productivity and creativity.Microsoft Surface Laptop 4 is a powerful and versatile laptop designed for productivity and creativity."
-  },
-  {
-    id: 7,
-    name: "Infinix INBOOK",
-    price: 10999,
-    brand: "Infinix",
-    color: "Black",
-    image: "https://dummyjson.com/image/i/products/7/1.jpg",
-    description: "Infinix INBOOK is a powerful and versatile laptop designed for productivity and creativity.Infinix INBOOK is a powerful and versatile laptop designed for productivity and creativity.Infinix INBOOK is a powerful and versatile laptop designed for productivity and creativity."
-  },
-  {
-    id: 8,
-    name: "HP Pavilion 15-DK1056WM",
-    price: 109990,
-    brand: "HP",
-    color: "Silver",
-    image: "https://dummyjson.com/image/i/products/8/1.jpg",
-    description: "HP Pavilion 15-DK1056WM is a powerful and versatile laptop designed for productivity and creativity.HP Pavilion 15-DK1056WM is a powerful and versatile laptop designed for productivity and creativity.HP Pavilion 15-DK1056WM is a powerful and versatile laptop designed for productivity and creativity."
-  },
-  {
-    id: 9,
-    name: "ASUS ROG Strix G15",
-    price: 159999,
-    brand: "ASUS",
-    color: "Black",
-    image: "https://dummyjson.com/image/i/products/9/1.jpg",
-    description: "ASUS ROG Strix G15 is a powerful and versatile laptop designed for gaming and productivity.ASUS ROG Strix G15 is a powerful and versatile laptop designed for gaming and productivity.ASUS ROG Strix G15 is a powerful and versatile laptop designed for gaming and productivity."
-  }
-]
+import { mobiles as initialData } from "./data";
 
 function App() {
   const [products, setProducts] = useState(initialData);
@@ -102,43 +19,24 @@ function App() {
           <Link to="/admin"> Admin Panel</Link>
         </nav>
 
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={(props) => <ProductList {...props} products={products} />}
-          />
+        <Routes>
+          <Route path="/" element={<ProductList products={products} />} />
 
           <Route
             path="/products/:id"
-            render={(props) => (
-              <ProductDetails {...props} products={products} />
-            )}
+            element={<ProductDetails products={products} />}
           />
 
           <Route
-            exact
             path="/admin"
-            render={(props) => (
-              <AdminPanel
-                {...props}
-                products={products}
-                setProducts={setProducts}
-              />
-            )}
+            element={<AdminPanel products={products} setProducts={setProducts} />}
           />
 
           <Route
             path="/admin/edit/:id"
-            render={(props) => (
-              <EditProduct
-                {...props}
-                products={products}
-                setProducts={setProducts}
-              />
-            )}
+            element={<EditProduct products={products} setProducts={setProducts} />}
           />
-        </Switch>
+        </Routes>
       </div>
     </BrowserRouter>
   );
